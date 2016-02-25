7676d5b3422f6ce5bfeee3d733ad60f7d4094f76
Justin Harringa <justin@harringa.com>
2016-02-25 11:31:35 -0600

    Add .gitignore, add education, " -> '

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,29 +1,64 @@
 var bio = {
-    "name": "Justin Harringa",
-    "role": "Senior Full-Stack Developer",
-    "contacts": {
-        "mobile": "+1 (708) 320-1337",
-        "email": "justin@harringa.com",
-        "github": "https://github.com/justinharringa",
-        "twitter": "https://twitter.com/justinharringa",
-        "location": "Denver, IA"
+    'name': 'Justin Harringa',
+    'role': 'Senior Full-Stack Developer',
+    'contacts': {
+        'mobile': '+1 (708) 320-1337',
+        'email': 'justin@harringa.com',
+        'github': 'https://github.com/justinharringa',
+        'twitter': 'https://twitter.com/justinharringa',
+        'location': 'Denver, IA'
     },
-    "welcomeMessage": "Established an accomplishments-driven career highlighted by rapid acceleration to increasingly responsible positions while building a vast technical skill set. Loves to learn and automate.",
-    "skills": [
-        "HTML",
-        "CSS",
-        "Javascript",
-        "Bootstrap",
-        "jQuery",
-        "AngularJS",
-        "Java",
-        "Ruby",
-        "NodeJS",
-        "PHP"
+    'welcomeMessage': 'Established an accomplishments-driven career highlighted by rapid acceleration to increasingly responsible positions while building a vast technical skill set. Loves to learn and automate.',
+    'skills': [
+        'HTML',
+        'CSS',
+        'Javascript',
+        'Bootstrap',
+        'jQuery',
+        'AngularJS',
+        'Java',
+        'Ruby',
+        'NodeJS',
+        'PHP'
     ],
-    "biopic": "some url???"
+    'biopic': 'some url???'
 };
-bio.display = function() {
+bio.display = function () {
 
 };
 
+var education = {
+    'schools': [
+        {
+            'name': 'University of Northern Iowa',
+            'location': 'Cedar Falls, IA',
+            'degree': 'Bachelor of Science',
+            'majors': [
+                'Computer Science'
+            ],
+            'dates': 2003,
+            'url': 'https://www.uni.edu'
+        },
+        {
+            'name': 'University of Iowa',
+            'location': 'Iowa City, IA',
+            'degree': 'Masters',
+            'majors': [
+                'Business Administration'
+            ],
+            'dates': 2010,
+            'url': 'http://www.uiowa.edu'
+        }
+    ],
+    'onlineCourses': [
+        {
+            'title': '',
+            'school': '',
+            'date': 2015,
+            'url': ''
+        }
+    ]
+};
+education.display = function () {
+
+};
