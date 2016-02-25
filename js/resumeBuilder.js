09c256c3cd083030e3f946aae795143695e7e410
Justin Harringa <justin@harringa.com>
2016-02-25 11:15:10 -0600

    Add bio to resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,29 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name": "Justin Harringa",
+    "role": "Senior Full-Stack Developer",
+    "contacts": {
+        "mobile": "+1 (708) 320-1337",
+        "email": "justin@harringa.com",
+        "github": "https://github.com/justinharringa",
+        "twitter": "https://twitter.com/justinharringa",
+        "location": "Denver, IA"
+    },
+    "welcomeMessage": "Established an accomplishments-driven career highlighted by rapid acceleration to increasingly responsible positions while building a vast technical skill set. Loves to learn and automate.",
+    "skills": [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Bootstrap",
+        "jQuery",
+        "AngularJS",
+        "Java",
+        "Ruby",
+        "NodeJS",
+        "PHP"
+    ],
+    "biopic": "some url???"
+};
+bio.display = function() {
+
+};
+
